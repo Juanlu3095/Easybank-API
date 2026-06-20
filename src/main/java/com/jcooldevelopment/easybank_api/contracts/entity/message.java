d5c60a7ded9bbc394b252c1fd44929e386571191
Juanlu3095 <jcooldevelopment@gmail.com>
2026-06-21 00:20:37 +0200

    Add message entity

---
+++ src/main/java/com/jcooldevelopment/easybank_api/contracts/entity/message.java
@@ -0,0 +1,44 @@
+package com.jcooldevelopment.easybank_api.contracts.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class message {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private UUID id;
+
+    @NotBlank(message="Name cannot be blank.")
+    @Column(name="name", nullable=false, length=50)
+    private String name;
+
+    @NotBlank(message="Surname cannot be blank.")
+    @Column(name="surname", nullable=false, length=100)
+    private String surname;
+
+    @Email(regexp="^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message="Email format is not correct.")
+    @Column(name="email", nullable=false, length=50, unique=true)
+    private String email;
+
+    @NotBlank(message="Phone cannot be blank.")
+    @Column(name="phone", nullable=false, length=50)
+    private String phone;
+
+    @NotBlank(message="Message cannot be blank.")
+    @Column(name="message", nullable=false)
+    private String message;
+}
