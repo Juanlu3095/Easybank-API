72b12b0560edcb964dfbd933ec78212280186a2c
Juanlu3095 <jcooldevelopment@gmail.com>
2026-08-28 01:24:34 +0200

    HTML Template for operation receipt

--- src/main/java/com/jcooldevelopment/easybank_api/specs/operation/OperationSpecs.java
+++ src/main/java/com/jcooldevelopment/easybank_api/specs/operation/OperationSpecs.java
@@ -9,14 +9,16 @@ import jakarta.persistence.criteria.Join;
 
 // https://blog.codmind.com/filtros-dinamicos-en-spring-boot-mediante/
 // https://localhorse.net/article/spring-boot-consultas-personalizadas-con-jpql-y-criteria-api
+// https://docs.spring.io/spring-data/jpa/reference/jpa/specifications.html
 // https://medium.com/@miguel.duque7/how-to-use-spring-data-jpa-specifications-to-filter-sql-queries-with-join-tables-1e821178c76b
+// Joins with specification: https://www.baeldung.com/spring-jpa-joining-tables
 // Specification cannot be null error: https://stackoverflow.com/questions/60009797/jpa-specification-and-null-parameter-in-where-clause
 public class OperationSpecs {
 
     public static Specification<Operation> findByConcept(String concept){
         return (root, query, criteriaBuilder) -> {
             if(concept.isEmpty()){
-                return criteriaBuilder.conjunction();
+                return criteriaBuilder.conjunction(); // if concept is "" it ignores this query
             }
             return criteriaBuilder.equal(root.get("concept"), concept);
         };
@@ -55,6 +57,7 @@ public class OperationSpecs {
             if(counterpartIban.isEmpty() || bankCode.isBlank()){
                 return criteriaBuilder.conjunction();
             }
+            // If account's bank code is not equal to our bank we use counterpart account external
             if(!counterpartIban.substring(4, 8).equals(bankCode)) {
                 return criteriaBuilder.equal(root.get("counterpartExternalAccount"), counterpartIban);
             } else {
